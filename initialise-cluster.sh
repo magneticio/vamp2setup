01325ca08793675cc2362e2c117546ab291e7c34
Alessandro Valcepina <alessandro@magnetic.io>
2018-09-28 09:19:47 +0200

    Improved initialise script so that it also returns the data required to set up the cluster in vamp

--- initialise-cluster.sh
+++ initialise-cluster.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
 kubectl apply -f ./templates/vamp-namespace-in-cluster.yaml
+
+SECRET_NAME=$( kubectl get sa default -n=vamp-system -o go-template='{{(index .secrets 0).name}}' )
+
+echo Cluster URL: $(kubectl cluster-info | awk 'NR==1{print $6}')
+echo
+echo Certificate: $(kubectl get secret ${SECRET_NAME} -n=vamp-system -o go-template='{{index .data "ca.crt"}}')
+echo
+echo Token: $(kubectl get secret ${SECRET_NAME} -n=vamp-system -o go-template='{{index .data.token }}')
